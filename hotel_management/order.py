0bf6eed12f43195fd3ddedf7dc75f7e5e7c72179
DoctorChe <duncan.reg@yandex.ru>
2020-02-12 00:13:53 +0300

    strategy pattern is working for payment

--- hotel_management/order.py
+++ hotel_management/order.py
@@ -1,9 +1,12 @@
+import abc
+
+
 class OrderItem:
     def __init__(self, item):
         self.item = item
 
-    def get_sum(self):
-        return self.item.price * self.item.get_time_period
+    def get_sum(self, check_in_date, check_out_date):
+        return self.item.price * self.item.get_time_period_impl(check_in_date, check_out_date)
 
 
 class Order:
@@ -26,10 +29,14 @@ class Order:
         return self._check_out_date
 
     def get_total(self):
-        return sum([item.get_sum() for item in self._order_items])
+        return sum([item.get_sum(self.check_in_date, self.check_out_date) for item in self._order_items])
 
     def add_discount(self, discount):
         pass
+
+    def pay(self, strategy):
+        total = self.get_total()
+        return strategy.pay(total)
 
 
 class OrderBuilder:
@@ -44,9 +51,45 @@ class OrderBuilder:
         self.order._check_out_date = date
         return self
 
-    def add_order_items(self, order_item):
-        self.order.order_items.append(order_item)
+    def set_order_items(self, order_items):
+        self.order._order_items = order_items
         return self
 
     def build(self):
         return self.order
+
+
+# Strategy pattern
+class PaymentStrategy(metaclass=abc.ABCMeta):
+    @abc.abstractmethod
+    def pay(self, amount):
+        pass
+
+
+class Cash:
+    pass
+
+
+class CashPaymentStrategy(PaymentStrategy):
+    def __init__(self, cash):
+        # self.cash = cash
+        pass
+
+    def pay(self, amount):
+        return f'Processing {amount} in cash'
+
+
+class CreditCard:
+    def __init__(self, number):
+        self._number = number
+
+    def get_number(self):
+        return self._number
+
+
+class CreditCardPaymentStrategy(PaymentStrategy):
+    def __init__(self, card):
+        self.card = card
+
+    def pay(self, amount):
+        return f'Processing {amount} via credit card {self.card.get_number()}'
